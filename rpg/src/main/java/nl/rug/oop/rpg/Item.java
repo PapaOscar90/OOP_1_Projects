29e209d9469c5cee14a414be9d3fdca114bc8496
Hydro7 <said.faroghi@gmail.com>
2017-04-30 17:43:09 +0400

    Turned item and weapon classes into abstract classes.

--- rpg/src/main/java/nl/rug/oop/rpg/Item.java
+++ rpg/src/main/java/nl/rug/oop/rpg/Item.java
@@ -3,8 +3,7 @@ package nl.rug.oop.rpg;
 /**
  * Created by PhilO on 30-Apr-17.
  */
-public class Item extends Inspectable {
-
+abstract class Item extends Inspectable {
     // An item has a description
     public Item(String s){
         super(s);
--- rpg/src/main/java/nl/rug/oop/rpg/Weapon.java
+++ rpg/src/main/java/nl/rug/oop/rpg/Weapon.java
@@ -3,11 +3,11 @@ package nl.rug.oop.rpg;
 /**
  * Created by PhilO on 30-Apr-17.
  */
-public class Weapon extends Item {
+abstract class Weapon extends Item {
     private int damage;
-
     // The weapon description goes to item, and the weapon has a damage rating
-    public Weapon(String s){
+    public Weapon(String s, int damage){
         super(s);
+        this.damage = damage;
     }
 }
