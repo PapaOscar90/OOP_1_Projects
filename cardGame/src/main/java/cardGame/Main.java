68ecd11252d7a178b43c4c8a46af911b067ebae4
Phil Oetinger <p.oetinger@student.rug.nl>
2017-05-15 14:40:25 +0200

    Hello World Commit

--- cardGame/src/main/java/cardGame/Main.java
+++ cardGame/src/main/java/cardGame/Main.java
@@ -8,3 +8,17 @@
 
 
 package cardGame;
+
+import cardGame.View.WindowFrame;
+
+import javax.swing.*;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello World.");
+        WindowFrame window = new WindowFrame();
+        window.setVisible(true);
+        window.setDefaultCloseOperation(window.EXIT_ON_CLOSE);
+
+    }
+}
---
+++ cardGame/src/main/java/cardGame/View/WindowFrame.java
@@ -0,0 +1,19 @@
+package cardGame.View;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by PhilO on 15-May-17.
+ */
+public class WindowFrame extends JFrame {
+    JButton button1 = new JButton("Clickme!");
+    JTextField textField1 = new JTextField("Default text");
+
+    public void WindowFrame(){
+        this.setSize(200,100);
+        this.getContentPane().setLayout(new FlowLayout());
+        this.getContentPane().add(button1);
+        this.getContentPane().add(textField1);
+    }
+}
